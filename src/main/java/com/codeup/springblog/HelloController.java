b13eb7a372ace84fbaaacc3bccf107835c44c2b0
Larry Castillo <larrycastillo2009@gmail.com>
2020-07-31 10:51:41 -0500

    lecture exercise completed

---
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -0,0 +1,33 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+    @Controller
+    class HelloController {
+
+        @GetMapping("/hello")
+        @ResponseBody
+        public String hello() {
+            return "Hello from Spring!";
+        }
+
+        @GetMapping("/goodbye")
+        @ResponseBody
+        public String goodbye(){
+            return "Goodbye from Spring!";
+        }
+
+        @GetMapping("")
+
+        @GetMapping("/books/{id}")
+        @ResponseBody
+        public String getBook(@PathVariable  long id){
+            return "Viewing book " + if
+        }
+
+
+    }
+
