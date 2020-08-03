ce0e12d2165c554eb2f0deb74c2f00bc32ffa931
Larry Castillo <larrycastillo2009@gmail.com>
2020-08-03 14:45:36 -0500

    update

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,22 +1,34 @@
 package com.codeup.springblog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+@Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String posts() {
-        return "This is the post page!";
+    public String posts(Model model) {
+        ArrayList<Post> posts = new ArrayList<>();
+        Post post = new Post("hello1","hello1");
+        Post post2 = new Post("hello2","hello2");
+        Post post3 = new Post("hello3","hello3");
+        posts.add(post);
+        posts.add(post2);
+        posts.add(post3);
+        model.addAttribute("posts",posts);
+        return "/posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String postsId(@PathVariable int id) {
-        return "This is " + id + "the post page!";
+    public String postsId(@PathVariable int id, Model model) {
+        Post post = new Post("hello", "hello world");
+        model.addAttribute("post",post);
+        return "/posts/show";
     }
 
     @GetMapping("/posts/create")
